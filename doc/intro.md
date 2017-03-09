dae45001b3351459d38f491274229486b2692433
James Oldfield <hi@james-oldfield.co>
2017-03-09 15:02:08 +0000

    add bem utilities

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to bem-classnames
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/bem_classnames/core.clj
@@ -0,0 +1,26 @@
+(ns bem-classnames.core
+  (:require [clojure.string :as s]))
+
+(defn- bem
+  [block
+   element
+   modifiers]
+  (cond
+    (nil? element) block
+    (empty? modifiers) (str block "__" element)
+    :else (s/join " " (map
+                        #(str block "__" element "--" (name %))
+                        (keys (filter (comp true? last) modifiers))))))
+
+(defn block
+  ([block] (partial bem block))
+  ([block element] (partial bem block element))
+  ([block element modifiers] (bem block element modifiers)))
+
+(defn -main
+  [& args]
+  (let [b (block "goofy")
+        be (block "goofy" "bob")]
+   (pr (be {:a true
+            :b (> 5 0)
+            :c false}))))
