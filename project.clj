d527ea9ca3c04ccf73dd75e75313a37dd61c7af7
James Oldfield <hi@james-oldfield.co>
2017-03-10 15:56:09 +0000

    update project.clj settings

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject bem-classnames "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :main ^:skip-aot bem-classnames.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+ :description "Small utility library for generating BEM-style classnames"
+ :url "https://github.com/James-Oldfield/bem-classnames"
+ :license {:name "MIT"
+           :url ""}
+ :dependencies [[org.clojure/clojure "1.8.0"]]
+ :main ^:skip-aot bem-classnames.core
+ :target-path "target/%s"
+ :profiles {:uberjar {:aot :all}})
